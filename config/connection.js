869c9dbf41852c0166ac849563e4f6702d715b8c
Alex Barron <abarron4544@yahoo.com>
2019-03-07 01:57:15 -0700

    working burger

---
+++ config/connection.js
@@ -0,0 +1,27 @@
+var mysql = require('mysql');
+
+// For jawsDB
+var connection;
+if (process.env.JAWSDB_URL) {
+    // DB is JawsDB on Heroku
+    connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+    // DB is local on localhost
+    connection = mysql.createConnection({
+        port: 8889,
+        host: 'localhost',
+        user: 'root',
+        password: 'root',
+        database: 'burgers_db'
+    });
+}
+
+connection.connect(function(err) {
+    if (err) {
+        console.error('error connecting: ' + err.stack);
+        return;
+    }
+    console.log('connected as id ' + connection.threadId);
+});
+
+module.exports = connection;
